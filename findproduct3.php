d0fea4a98d59e9e5d3e38cb219887f4850b43096
Hasin Hayder <countdraculla@gmail.com>
2020-04-26 19:58:58 +0600

    Cleanup

--- findproduct3.php
+++ findproduct3.php
@@ -7,7 +7,8 @@ $usersRepo = $entityManager->getRepository(User::class);
 $users = $usersRepo->findAll();
 foreach($users as $user){
     echo "User Name: ".$user->getName().PHP_EOL;
-    foreach($user->getProducts() as $product){
+    $products = $user->getProducts();
+    foreach( $products as $product){
         echo sprintf("-%s\n", $product->getName());
     }
     echo "=========\n\n";
